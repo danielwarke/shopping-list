a278e7b80bc82e62a2a1fdee9021822ee5886bed
Daniel Warke <dwarke@hayesco.com>
2024-05-07 20:51:41 -0700

    fix: add form to sign up page so that enter key works properly

--- apps/client/src/pages/sign-up.tsx
+++ apps/client/src/pages/sign-up.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { apiClient } from "@/api/api-client";
 import { Alert, Box, Button, Container, TextField } from "@mui/material";
@@ -19,7 +19,9 @@ export default function SignUp() {
     onSuccess: () => setSuccess(true),
   });
 
-  function handleSubmit() {
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
     signUpMutation.mutate({
       name,
       email,
@@ -29,71 +31,74 @@ export default function SignUp() {
 
   return (
     <Container maxWidth="sm">
-      <Box marginTop="3vh" marginBottom="1em" sx={{ typography: "h5" }}>
-        Sign up to start creating your own shopping list
-      </Box>
-      <ErrorRenderer
-        isError={signUpMutation.isError}
-        error={signUpMutation.error}
-      />
-      {success && (
-        <Alert severity="success">
-          You have signed up successfully! <br />
-          Please check your email for a link to verify your account. You can
-          safely close this tab.
-        </Alert>
-      )}
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        flexDirection="column"
-        gap={2}
-        marginTop="3vh"
-      >
-        <TextField
-          label="Name"
-          fullWidth
-          placeholder="John Doe"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          disabled={success}
+      <form onSubmit={handleSubmit}>
+        <Box marginTop="3vh" marginBottom="1em" sx={{ typography: "h5" }}>
+          Sign up to start creating your own shopping list
+        </Box>
+        <ErrorRenderer
+          isError={signUpMutation.isError}
+          error={signUpMutation.error}
         />
-        <TextField
-          label="Email"
-          fullWidth
-          placeholder="jdoe@email.com"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          disabled={success}
-        />
-        <TextField
-          label="Password"
-          fullWidth
-          placeholder="Make it a good one"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          disabled={success}
-        />
-        {!success && (
-          <>
-            <LoadingButton
-              variant="contained"
-              size="large"
-              onClick={handleSubmit}
-              loading={signUpMutation.isPending}
-              disabled={!name || !email || !password}
-            >
-              Sign Up
-            </LoadingButton>
-            <Button onClick={() => router.push("/login")}>
-              Already have an account?
-            </Button>
-          </>
+        {success && (
+          <Alert severity="success">
+            You have signed up successfully! <br />
+            Please check your email for a link to verify your account. You can
+            safely close this tab.
+          </Alert>
         )}
-      </Box>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="column"
+          gap={2}
+          marginTop="3vh"
+        >
+          <TextField
+            label="Name"
+            fullWidth
+            placeholder="John Doe"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            disabled={success}
+          />
+          <TextField
+            label="Email"
+            fullWidth
+            placeholder="jdoe@email.com"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            disabled={success}
+          />
+          <TextField
+            label="Password"
+            fullWidth
+            placeholder="Make it a good one"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            disabled={success}
+          />
+          {!success && (
+            <>
+              <LoadingButton
+                variant="contained"
+                size="large"
+                onClick={handleSubmit}
+                type="submit"
+                loading={signUpMutation.isPending}
+                disabled={!name || !email || !password}
+              >
+                Sign Up
+              </LoadingButton>
+              <Button type="button" onClick={() => router.push("/login")}>
+                Already have an account?
+              </Button>
+            </>
+          )}
+        </Box>
+      </form>
     </Container>
   );
 }
