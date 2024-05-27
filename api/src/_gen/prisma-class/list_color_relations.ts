cafda587b96e5e8a0f6b1184d4ec3596a0075d5f
Daniel Warke <dwarke@hayesco.com>
2024-05-26 22:11:38 -0700

    chore: stop git tracking prisma class generated files

--- api/src/_gen/prisma-class/list_color_relations.ts
+++
@@ -1,7 +0,0 @@
-import { ShoppingList } from './shopping_list'
-import { ApiProperty } from '@nestjs/swagger'
-
-export class ListColorRelations {
-	@ApiProperty({ isArray: true, type: () => ShoppingList })
-	shoppingLists: ShoppingList[]
-}
